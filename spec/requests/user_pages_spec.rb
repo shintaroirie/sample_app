536c6130b3b0529315ad81bbe524180fc4555b9b
shintaro.irie <your.email@example.com>
2014-07-06 13:53:23 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,7 +12,14 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
-  describe "signup" do
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+
+describe "signup" do
 
     before { visit signup_path }
 
@@ -24,12 +31,27 @@ describe "User pages" do
       end
     end
 
+describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
